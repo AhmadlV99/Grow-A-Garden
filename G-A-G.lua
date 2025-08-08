dd0ab3a3285a7ef1a8a3197525971aabab447fd5
Ahmad99 <goofybloxbypasser@gmail.com>
2025-08-09 05:34:24 +0800

    Create G-A-G.lua

---
+++ G-A-G.lua
@@ -0,0 +1,6 @@
+local Games = {
+    [126884695634066] = "https://raw.githubusercontent.com/AhmadlV99/Grow-A-Garden/refs/heads/main/E-G-G-E-S-P-S-C-R-I-P-T.lua", -- Grow a Garden
+    [91867617264223] = "https://raw.githubusercontent.com/AhmadlV99/Grow-A-Garden/refs/heads/main/E-G-G-E-S-P-S-C-R-I-P-T.lua", -- Grow a Garden 1
+}
+
+return Games
